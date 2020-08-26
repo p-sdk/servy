c781908542ba968664e3a1e5feafd5da7cce2b14
p-sdk <paul.sdk@icloud.com>
2020-08-26 13:02:55 +0200

    Check the Content-Type header

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -10,7 +10,7 @@ defmodule Servy.Parser do
 
     headers = parse_headers(header_lines, %{})
 
-    params = parse_params(params_string)
+    params = parse_params(headers["Content-Type"], params_string)
 
     %Conv{
       method: method,
@@ -28,7 +28,9 @@ defmodule Servy.Parser do
 
   def parse_headers([], headers), do: headers
 
-  def parse_params(params_string) do
+  def parse_params("application/x-www-form-urlencoded", params_string) do
     params_string |> String.trim |> URI.decode_query
   end
+
+  def parse_params(_, _), do: %{}
 end
