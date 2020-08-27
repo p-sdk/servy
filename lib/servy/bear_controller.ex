1031c2a1d87e1a346fc8037973ef29e572020f6f
p-sdk <paul.sdk@icloud.com>
2020-08-27 22:25:08 +0200

    Create list of bears in the index function

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,6 +1,20 @@
 defmodule Servy.BearController do
+  alias Servy.Wildthings
+  alias Servy.Bear
+
+  defp bear_item(bear) do
+    "<li>#{bear.name} - #{bear.type}</li>"
+  end
+
   def index(conv) do
-    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+    items =
+      Wildthings.list_bears()
+      |> Enum.filter(&Bear.is_grizzly/1)
+      |> Enum.sort(&Bear.order_asc_by_name/2)
+      |> Enum.map(&bear_item/1)
+      |> Enum.join
+
+    %{ conv | status: 200, resp_body: "<ul>#{items}</ul>" }
   end
 
   def show(conv, %{"id" => id}) do
