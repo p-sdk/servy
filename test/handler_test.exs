ae188b8aeee8577851335ed5c276aed2db10ee45
p-sdk <paul.sdk@icloud.com>
2020-09-02 23:07:05 +0200

    Test the handler

---
+++ test/handler_test.exs
@@ -0,0 +1,275 @@
+defmodule HandlerTest do
+  use ExUnit.Case
+
+  import Servy.Handler, only: [handle: 1]
+
+  test "GET /wildthings" do
+    request = """
+    GET /wildthings HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = handle(request)
+
+    assert response == """
+    HTTP/1.1 200 OK\r
+    Content-Type: text/html\r
+    Content-Length: 20\r
+    \r
+    Bears, Lions, Tigers
+    """
+  end
+
+  test "GET /bears" do
+    request = """
+    GET /bears HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = handle(request)
+
+    expected_response = """
+    HTTP/1.1 200 OK\r
+    Content-Type: text/html\r
+    Content-Length: 356\r
+    \r
+    <h1>All The Bears!</h1>
+
+    <ul>
+      <li>Brutus - Grizzly</li>
+      <li>Iceman - Polar</li>
+      <li>Kenai - Grizzly</li>
+      <li>Paddington - Brown</li>
+      <li>Roscoe - Panda</li>
+      <li>Rosie - Black</li>
+      <li>Scarface - Grizzly</li>
+      <li>Smokey - Black</li>
+      <li>Snow - Polar</li>
+      <li>Teddy - Brown</li>
+    </ul>
+    """
+
+    assert remove_whitespace(response) == remove_whitespace(expected_response)
+  end
+
+  test "GET /bigfoot" do
+    request = """
+    GET /bigfoot HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = handle(request)
+
+    assert response == """
+    HTTP/1.1 404 Not Found\r
+    Content-Type: text/html\r
+    Content-Length: 17\r
+    \r
+    No /bigfoot here!
+    """
+  end
+
+  test "GET /bears/1" do
+    request = """
+    GET /bears/1 HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = handle(request)
+
+    expected_response = """
+    HTTP/1.1 200 OK\r
+    Content-Type: text/html\r
+    Content-Length: 74\r
+    \r
+    <h1>Show Bear</h1>
+    <p>
+    Is Teddy hibernating? <strong>true</strong>
+    </p>
+    """
+
+    assert remove_whitespace(response) == remove_whitespace(expected_response)
+  end
+
+  test "GET /wildlife" do
+    request = """
+    GET /wildlife HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = handle(request)
+
+    assert response == """
+    HTTP/1.1 200 OK\r
+    Content-Type: text/html\r
+    Content-Length: 20\r
+    \r
+    Bears, Lions, Tigers
+    """
+  end
+
+  test "DELETE /bears/1" do
+    request = """
+    DELETE /bears/1 HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = handle(request)
+
+    assert response == """
+    HTTP/1.1 403 Forbidden\r
+    Content-Type: text/html\r
+    Content-Length: 29\r
+    \r
+    Deleting a bear is forbidden!
+    """
+  end
+
+  test "GET /bears?id=1" do
+    request = """
+    GET /bears?id=1 HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = handle(request)
+
+    expected_response = """
+    HTTP/1.1 200 OK\r
+    Content-Type: text/html\r
+    Content-Length: 74\r
+    \r
+    <h1>Show Bear</h1>
+    <p>
+    Is Teddy hibernating? <strong>true</strong>
+    </p>
+    """
+
+    assert remove_whitespace(response) == remove_whitespace(expected_response)
+  end
+
+  test "GET /bears/new" do
+    request = """
+    GET /bears/new HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = handle(request)
+
+    expected_response = """
+    HTTP/1.1 200 OK\r
+    Content-Type: text/html\r
+    Content-Length: 230\r
+    \r
+    <form action="/bears" method="POST">
+      <p>
+        Name:<br/>
+        <input type="text" name="name">
+      </p>
+      <p>
+        Type:<br/>
+        <input type="text" name="type">
+      </p>
+      <p>
+        <input type="submit" value="Create Bear">
+      </p>
+    </form>
+    """
+
+    assert remove_whitespace(response) == remove_whitespace(expected_response)
+  end
+
+  test "GET /about" do
+    request = """
+    GET /about HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = handle(request)
+
+    expected_response = """
+    HTTP/1.1 200 OK\r
+    Content-Type: text/html\r
+    Content-Length: 27\r
+    \r
+    <h1>Wildthings Refuge</h1>
+    """
+
+    assert remove_whitespace(response) == remove_whitespace(expected_response)
+  end
+
+  test "GET /pages/about" do
+    request = """
+    GET /pages/about HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = handle(request)
+
+    expected_response = """
+    HTTP/1.1 200 OK\r
+    Content-Type: text/html\r
+    Content-Length: 27\r
+    \r
+    <h1>Wildthings Refuge</h1>
+    """
+
+    assert remove_whitespace(response) == remove_whitespace(expected_response)
+  end
+
+  test "POST /bears" do
+    request = """
+    POST /bears HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    Content-Type: application/x-www-form-urlencoded\r
+    Content-Length: 21\r
+    \r
+    name=Baloo&type=Brown
+    """
+
+    response = handle(request)
+
+    assert response == """
+    HTTP/1.1 201 Created\r
+    Content-Type: text/html\r
+    Content-Length: 33\r
+    \r
+    Created a Brown bear named Baloo!
+    """
+  end
+
+  defp remove_whitespace(text) do
+    String.replace(text, ~r{\s}, "")
+  end
+end
