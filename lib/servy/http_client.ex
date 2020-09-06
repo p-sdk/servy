82587ea9d9e04db3a6029369bbb0d2b71af5c95d
p-sdk <paul.sdk@icloud.com>
2020-09-06 15:39:32 +0200

    Add optional host and port parameters to HttpClient.send_request

--- lib/servy/http_client.ex
+++ lib/servy/http_client.ex
@@ -1,8 +1,7 @@
 defmodule Servy.HttpClient do
-  def send_request(request) do
-    some_host_in_net = 'localhost' # to make it runnable on one machine
+  def send_request(request, host \\ 'localhost', port \\ 4000) do
     {:ok, socket} =
-      :gen_tcp.connect(some_host_in_net, 4000, [:binary, packet: :raw, active: false])
+      :gen_tcp.connect(host, port, [:binary, packet: :raw, active: false])
     :ok = :gen_tcp.send(socket, request)
     {:ok, response} = :gen_tcp.recv(socket, 0)
     :ok = :gen_tcp.close(socket)
