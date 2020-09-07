09692c42be21694cbbcd3692dcf7928024d3ac90
p-sdk <paul.sdk@icloud.com>
2020-09-07 20:24:19 +0200

    Change HttpServerTest to send concurrent requests

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -4,11 +4,23 @@ defmodule HttpServerTest do
   alias Servy.HttpServer
 
   test "accepts a request on a socket and sends back a response" do
+    parent = self()
+    num_requests = 5
+
     spawn HttpServer, :start, [4001]
 
-    {:ok, response} = HTTPoison.get "http://localhost:4001/wildthings"
+    for _ <- 1..num_requests do
+      spawn fn ->
+        send parent, HTTPoison.get "http://localhost:4001/wildthings"
+      end
+    end
 
-    assert response.status_code == 200
-    assert response.body == "Bears, Lions, Tigers"
+    for _ <- 1..num_requests do
+      receive do
+        {:ok, response} ->
+          assert response.status_code == 200
+          assert response.body == "Bears, Lions, Tigers"
+      end
+    end
   end
 end
