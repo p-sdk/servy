3cfab9b0505141d8c4e88be8b3f88405d2cdea34
p-sdk <paul.sdk@icloud.com>
2020-09-07 22:52:26 +0200

    Use tasks to get location

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -20,13 +20,17 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def route(%Conv{method: "GET", path: "/snapshots"} = conv) do
+  def route(%Conv{method: "GET", path: "/sensors"} = conv) do
+    task = Task.async(fn -> Servy.Tracker.get_location "bigfoot" end)
+
     snapshots =
       ["cam-1", "cam-2", "cam-3"]
       |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
       |> Enum.map(&Task.await/1)
 
-    %{ conv | status: 200, resp_body: inspect(snapshots) }
+    where_is_bigfoot = Task.await task
+
+    %{ conv | status: 200, resp_body: inspect {snapshots, where_is_bigfoot} }
   end
 
   def route(%Conv{method: "GET", path: "/kaboom"}) do
