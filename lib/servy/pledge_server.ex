66fec013c2a939425f75e425e385622e91d998fb
p-sdk <paul.sdk@icloud.com>
2020-09-11 19:14:53 +0200

    Allow to set the cache size

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -3,11 +3,15 @@ defmodule Servy.PledgeServer do
 
   use GenServer
 
+  defmodule State do
+    defstruct cache_size: 3, pledges: []
+  end
+
   # Client Interface
 
   def start do
     IO.puts "Starting the pledge server ..."
-    GenServer.start(__MODULE__, [], name: @name)
+    GenServer.start(__MODULE__, %State{}, name: @name)
   end
 
   def create_pledge(name, amount) do
@@ -26,25 +30,34 @@ defmodule Servy.PledgeServer do
     GenServer.cast @name, :clear
   end
 
+  def set_cache_size(size) do
+    GenServer.cast @name, {:set_cache_size, size}
+  end
+
   # Server Callbacks
 
-  def handle_cast(:clear, _state) do
-    {:noreply, []}
+  def handle_cast(:clear, state) do
+    {:noreply, %{ state | pledges: [] }}
+  end
+
+  def handle_cast({:set_cache_size, size}, state) do
+    {:noreply, %{ state | cache_size: size }}
   end
 
   def handle_call({:create_pledge, name, amount}, _from, state) do
     {:ok, id} = send_pledge_to_service(name, amount)
-    most_recent_pledges = Enum.take(state, 2)
-    new_state = [ {name, amount} | most_recent_pledges ]
+    most_recent_pledges = Enum.take(state.pledges, state.cache_size - 1)
+    cached_pledges = [ {name, amount} | most_recent_pledges ]
+    new_state = %{ state | pledges: cached_pledges }
     {:reply, id, new_state}
   end
 
   def handle_call(:recent_pledges, _from, state) do
-    {:reply, state, state}
+    {:reply, state.pledges, state}
   end
 
   def handle_call(:total_pledged, _from, state) do
-    total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
+    total = Enum.map(state.pledges, &elem(&1, 1)) |> Enum.sum
     {:reply, total, state}
   end
 
